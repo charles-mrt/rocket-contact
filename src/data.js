ae12534fc450a82dbeed934617f2719cb37d4106
charles-mrt <charlesmartins.ads@gmail.com>
2023-04-20 18:02:16 -0300

    first-commit

---
+++ src/data.js
@@ -0,0 +1,113 @@
+export const contactData = {
+    "groups": {
+        "a": [
+            {
+                "id": 1,
+                "image-url": "abraao-sena",
+                "name": "Abraão Sena",
+                "phone-number": "(11) 90876-1234",
+            }
+        ],
+        "b": [
+            {
+                "id": 1,
+                "image-url": "beatriz-clasen",
+                "name": "Beatriz Clasen",
+                "phone-number": "(48) 90876-1123",
+            },
+        ],
+
+        "d": [
+            {
+                "id": 1,
+                "image-url": "diego-fernandes",
+                "name": "Diego Fernandes",
+                "phone-number": "(47) 90563-6222",
+            },
+            {
+                "id": 2,
+                "image-url": "daniel-duarte",
+                "name": "Daniel Duarte",
+                "phone-number": "(82) 90876-6534",
+            }
+        ],
+
+        "j": [
+            {
+                "id": 1,
+                "image-url": "jake-gracielly",
+                "name": "Jake Gracielly",
+                "phone-number": "(11) 90888-3412",
+            },
+        ],
+        "m": [
+            {
+                "id": 1,
+                "image-url": "mike-brito",
+                "name": "Mike Brito",
+                "phone-number": "(44) 90212-2122",
+            },
+        ],
+        "r": [
+            {
+                "id": 1,
+                "image-url": "rodrigo-goncalves",
+                "name": "Rodrigo Gonçalves",
+                "phone-number": "(1) 9033-21332",
+            },
+            {
+                "id": 2,
+                "image-url": "renato-mendes",
+                "name": "Renato Mendes",
+                "phone-number": "(21) 90876-8765",
+            },
+            {
+                "id": 3,
+                "image-url": "",
+                "name": "Raquel Ramires",
+                "phone-number": "(11) 90555-8265",
+            }
+        ],
+        "s": [
+            {
+                "id": 1,
+                "image-url": "samantha-silva",
+                "name": "Samantha Silva",
+                "phone-number": "(11) 90876-1234",
+            },
+            {
+                "id": 2,
+                "image-url": "",
+                "name": "Samuel Santos",
+                "phone-number": "(21) 90876-2345",
+            },
+            {
+                "id": 3,
+                "image-url": "stephanie-souza",
+                "name": "Stephanie Souza",
+                "phone-number": "(48) 90555-5544",
+            }
+        ],
+        "t": [
+            {
+                "id": 1,
+                "image-url": "thiago-torres",
+                "name": "Thiago Torres",
+                "phone-number": "(11) 90876-3456",
+            },
+            {
+                "id": 2,
+                "image-url": "",
+                "name": "Talita Teixeira",
+                "phone-number": "(21) 90876-4567",
+            },
+            {
+                "id": 3,
+                "image-url": "",
+                "name": "Tiago Tavares",
+                "phone-number": "(48) 90555-6677",
+            }
+        ]
+
+    }
+}
